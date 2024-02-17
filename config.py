3d5036a79cd30ed6c937a587ac26c1eeabd08e50
Alex36r <160328692+Alex36r@users.noreply.github.com>
2024-02-17 16:23:46 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+token = '6744469943:AAE4UdrosLyHw2eEBc_Odl408pI-7cVrhvQ' # Токен бота
+loginbot = 'Glass1Boga_bot' # Логин бота телеграм
+
+QIWI_PUBLICKEY = 'xxxxxx' #p2p public key
+QIWI_TOKEN = '1935285a8e4a31c25e5bf0e0b72d74a6' # токен киви
+QIWI_NUMBER = '+79518703232' # Номер киви без +
+
+admin_id = 6876906190 # Ваш айди
+
+price = 190 # Цена за доступ
